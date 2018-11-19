ea93f0e95666f81ed37c8eb4d9592eeeed4f9577
Bill <billkariri@gmail.com>
2018-11-19 23:26:32 +0300

    feat: duplicate event listener for second cat

--- Js/cat.js
+++ Js/cat.js
@@ -1,11 +1,21 @@
 (function(){
+	let imageClicks = document.querySelector('.first');
+	let imageClicks2 = document.querySelector('.second');
 	let clicks = 0;
-	let imageClicks = document.querySelector('img');
-	let display = document.querySelector('p');
-	
-	imageClicks.addEventListener('click',()=>{
+	let clicks2 = 0;
+	function clickedItems(){
+		let display = document.querySelector('.cat1');
 		clicks++;
-		display.innerHTML = `You have clicked this cat for ${parseInt(clicks)} times`;	
-	},false);
+		display.innerHTML = `You have clicked Cat 1 for ${parseInt(clicks)} times`;	
+	}
+	imageClicks.addEventListener('click',clickedItems);
+
+	function clickedItems2(){
+		let display = document.querySelector('.cat2');
+		clicks2++;
+		display.innerHTML = `You have clicked Cat 2 for ${parseInt(clicks2)} times`;	
+	}
+	imageClicks2.addEventListener('click',clickedItems2);
+
 	
 }());
